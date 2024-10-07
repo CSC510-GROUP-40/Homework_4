f293fb81b40c72e8f12e36b63c990b5246ba221b
Lingjun Liu <rienshaliu@gmail.com>
2024-10-07 17:26:21 -0400

    edit task2 into a single pipeline

--- task2.sh
+++ task2.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
 
-echo "Step 3.a:"
-grep -l 'sample' dataset1/* | xargs grep -l 'CSC510' | xargs grep -oE "CSC510" | uniq -c | sed 's/:CSC510//g' | grep ' [3-9]\| [1-9][0-9]+' | while read -r count file; do
-    echo "File: $file, Count: $count"
-done
-
-echo "Step 3.b and 3.c:"
+# It should be run by MacOS
+# If run with Linux, you need to replace -f%z with --format=%s
 grep -l 'sample' dataset1/* | xargs grep -l 'CSC510' | xargs grep -oE "CSC510" | uniq -c | sed 's/:CSC510//g' | grep ' [3-9]\| [1-9][0-9]+' | gawk '{
-    "stat -f%z " $2 | getline size;  # Get the file size
-    print $2, $1, size               # Print the file, occurrences, and file size
-}' | sort -k2,2nr -k3,3nr | while read -r file count size; do
-    echo "File: $file, Count: $count, Size: $size"
-
-    new_file=$(echo "$file" | sed 's/file_/filtered_/')
-    
-    # rename the file
-    mv "$file" "$new_file"
-    
-    echo "Renamed $file to $new_file"
-done
+    "stat -f%z " $2 | getline size;                             # Get the file size
+    print $2, $1, size                                          # Print the file, occurrences, and file size
+}' | sort -k2,2nr -k3,3nr | gawk '{
+    "echo " $1 " | sed s/file_/filtered_/" | getline new_file;  # Use sed to generate the new file name
+    "mv " $1 " " new_file                                       # Rename the file
+    print new_file                                              # Print the new files
+}'
