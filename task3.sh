a8bc0b3f6521e0d5ab48f33deab618d5d0f7dc7a
kofiarkoh <kofiarkoh@gmail.com>
2024-10-10 19:24:37 -0400

    merge commands for task 3

--- task3.sh
+++ task3.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-sed 's/\(.*\),/\1,~/' titanic.csv > titanic_fixed_nulls.csv # replace the last comma it exist with ~
-sed 's/,,/,~,/g' titanic.csv > titanic_modified.csv #fixes other missing nulls
-sed 's/female/F/g' titanic.csv > titanic_modified.csv # replace female with F
-sed 's/male/M/g' titanic_modified.csv > titanic_final.csv # replace male with M
-
-# Path to the input CSV file
-input_file="titanic_final.csv"  # Replace this with your actual input file
-
-# Run the gawk script
-#/opt/homebrew/bin/gawk -f task3.gawk "$input_file"
-gawk -f task3.gawk "$input_file"
+sed 's/\(.*\),/\1,~/' titanic.csv | sed 's/,,/,~,/g' | sed 's/female/F/g' | sed 's/male/M/g' | gawk -f task3.gawk
